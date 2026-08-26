091ac3bdd37de4a60dc42e51b29f6d996d6138a3
Softlink <arielgiovanni90@gmail.com>
2026-08-26 15:29:46 -0600

    termine el ejemplo que me faltaba

---
+++ Ejemplos/ej2/Principal.java
@@ -0,0 +1,8 @@
+package Ejemplos.ej2;
+
+public class Principal {
+    public static void main(String[] args) {
+        Coche c = new Coche("Toyota");
+        System.out.println("Marca: " + c.marca);
+ }
+}
