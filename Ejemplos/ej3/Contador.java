d4979f45410473f7b77738dedab001eb1ada0269
mz3760012024-maker <mz3760012024@unab.edu.sv>
2026-08-26 21:42:44 +0000

    ayuda para softlink ej3

---
+++ Ejemplos/ej3/Contador.java
@@ -0,0 +1,8 @@
+package Ejemplos.ej3;
+
+public class Contador {
+    int valor = 0;
+    public void incrementar() {
+        valor++;
+    }
+}
---
+++ Ejemplos/ej3/Principal.java
@@ -0,0 +1,13 @@
+package Ejemplos.ej3;
+
+public class Principal {
+    public static void main(String[] args) {
+        Contador c1 = new Contador();
+        Contador c2 = new Contador();
+        c1.incrementar();
+        c1.incrementar();
+        c2.incrementar();
+        System.out.println("c1: " + c1.valor);
+        System.out.println("c2: " + c2.valor);
+    }
+}
